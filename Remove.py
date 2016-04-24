271a07823db035455d658582b80c549ae3102a85
SarahPujol <spujol@macalester.edu>
2016-04-24 15:35:59 -0500

    fixed remove

--- Remove.py
+++ Remove.py
@@ -2,6 +2,43 @@ import copy
 import random
 import Generation
 
+def remove_numbers(num, difficulty):
+    grid = Generation.make_full_grid(num)
+    count = 0
+    additions = random.randint(min_remove(difficulty), max_remove(difficulty))
+    tempgrid = copy.copy(grid)
+    spots = [x for x in range(0, 9)]
+    spots2 = [x for x in range(0, 9)]
+    while count < additions:
+        rando = random.choice(spots)
+        rando2 = random.choice(spots2)
+        if tempgrid[rando][rando2] is not 0:
+            tempgrid[rando][rando2] = 0
+            if is_solvable(tempgrid):
+                colcount = 0
+                for x in range(0, 9):
+                    if tempgrid[rando][x] == 0:
+                        colcount += 1
+                rowcount = 0
+                for x in range(0, 9):
+                    if tempgrid[x][rando2] == 0:
+                        rowcount += 1
+                if colcount <= lowerbound(difficulty) and rowcount <= lowerbound(difficulty):
+                    grid = copy.copy(tempgrid)
+                    count += 1
+                elif colcount <= lowerbound(difficulty):
+                    spots2.remove(rando2)
+                    tempgrid = copy.copy(grid)
+                else:
+                    spots.remove(rando)
+                    tempgrid = copy.copy(grid)
+
+    return grid
+
+
+def is_solvable(grid):
+    return True
+
 def min_remove(difficulty):
     if difficulty == 1:
         return 32
@@ -22,36 +59,6 @@ def max_remove(difficulty):
         return 53
     if difficulty == 4:
         return 59
-
-def remove_numbers(num, difficulty):
-    grid = Generation.make_full_grid(num)
-    count = 0
-    additions = random.randint(min_remove(difficulty), max_remove(difficulty))
-    tempgrid = copy.copy(grid)
-    while count < additions:
-        rando = random.randint(0, 8)
-        rando2 = random.randint(0, 8)
-        if tempgrid[rando][rando2] is not 0:
-            tempgrid[rando][rando2] = 0
-            if is_solvable(tempgrid):
-                colcount = 0
-                for x in range(0, 9):
-                    if tempgrid[rando][x] == 0:
-                        colcount += 1
-                rowcount = 0
-                for x in range(0, 9):
-                    if tempgrid[x][rando2] == 0:
-                        rowcount += 1
-                if colcount <= lowerbound(difficulty) and rowcount <= lowerbound(difficulty):
-                    grid = copy.copy(tempgrid)
-                    count += 1
-                else:
-                    tempgrid = copy.copy(grid)
-    return grid
-
-
-def is_solvable(grid):
-    return True
 
 def lowerbound(difficulty):
     if difficulty == 1:
