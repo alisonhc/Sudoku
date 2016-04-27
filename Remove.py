efabaa1dda91c5321d9a22215522a63961d1c3c2
SarahPujol <spujol@macalester.edu>
2016-04-27 08:43:47 -0500

    fixed remove

--- Remove.py
+++ Remove.py
@@ -8,34 +8,46 @@ def remove_numbers(num, difficulty):
     count = 0
     additions = random.randint(min_remove(difficulty), max_remove(difficulty))
     tempgrid = copy.copy(grid)
-    spots = [x for x in range(0, 9)]
-    spots2 = [x for x in range(0, 9)]
+    spotsr = [x for x in range(0, 9)]
+    spotsc = [x for x in range(0, 9)]
     while count < additions:
-        rando = random.choice(spots)
-        rando2 = random.choice(spots2)
-        if tempgrid[rando][rando2] is not 0:
-            tempgrid[rando][rando2] = 0
+        randor = random.choice(spotsr)
+        randoc = random.choice(spotsc)
+        if tempgrid[randor][randoc] is not 0:
+            removed_number = tempgrid[randor][randoc]
+            tempgrid[randor][randoc] = 0
+            count += 1
             if is_solvable(tempgrid):
-                colcount = 0
-                for x in range(0, 9):
-                    if tempgrid[rando][x] == 0:
-                        colcount += 1
-                rowcount = 0
-                for x in range(0, 9):
-                    if tempgrid[x][rando2] == 0:
-                        rowcount += 1
-                if colcount <= lowerbound(difficulty) and rowcount <= lowerbound(difficulty):
-                    grid = copy.copy(tempgrid)
-                    count += 1
-                if colcount > lowerbound(difficulty):
-                    spots2.remove(rando2)
-                    tempgrid = copy.copy(grid)
-                if rowcount > lowerbound(difficulty):
-                    spots.remove(rando)
-                    tempgrid = copy.copy(grid)
+                row_count, col_count = col_and_row_counts(tempgrid, randor, randoc)
+                if col_count > lowerbound(difficulty) or row_count > lowerbound(difficulty):
+                    if col_count > lowerbound(difficulty):
+                        spotsc.remove(randoc)
+                    if row_count > lowerbound(difficulty):
+                        spotsr.remove(randor)
+                    tempgrid[randor][randoc] = removed_number
+        #This is the code for checking if the grid can't delete any more spots.
+        zero_count = True
+        if count > min_remove(difficulty):
+            for r in spotsr:
+                for c in spotsc:
+                    if tempgrid[r][c] is not 0:
+                        zero_count = False
+            if zero_count:
+                count = additions
 
-    return grid
 
+    return tempgrid
+
+def col_and_row_counts(grid, row, col):
+    col_count = 0
+    row_count = 0
+    for x in range(0, 9):
+        if grid[row][x] == 0:
+            row_count += 1
+    for x in range(0, 9):
+        if grid[x][col] == 0:
+            col_count += 1
+    return row_count, col_count
 
 def is_solvable(grid):
     return True
@@ -72,4 +84,4 @@ def lowerbound(difficulty):
         return 9
 
 
-print(remove_numbers(3, 2))
+print(remove_numbers(3, 1))
